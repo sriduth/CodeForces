b6f3faf4cd879cbc99f6e0a1079e44a110255a55
Sriduth Jayhari <sriduth.jayhari@gmail.com>
2014-10-07 21:11:04 +0530

    Added Bayan B correct solution

--- 475/b.cpp
+++ 475/b.cpp
@@ -1,94 +1,98 @@
 #include <iostream>
 #include <cstring>
+
 using namespace std;
 
-char street_LR[20];
-char street_UD[20];
+int horizontal,vertical;
+char hor_streets[20],vert_streets[20];
+
 bool visited[20][20];
-int vcount;
 
-int x,y;
-
-void dfs(int a,int b)
+void search(int x,int y)
 {
-	if(visited[b][a] || a >= y || b >= x || a < 0 || b < 0)
+	if(visited[x][y])
 		return;
 
-	visited[b][a] = true;
-	vcount++;
+	if( (y < 0) || (x < 0) || (x >= horizontal) || (y >= vertical) )
+		return;
 
-	if(street_LR[b] == '>')
+	visited[x][y] = true;
+
+	if(hor_streets[x] == '>')
+		search(x,y+1);
+	if(hor_streets[x] == '<')
+		search(x,y-1);
+	if(vert_streets[y] == '^')
+		search(x-1,y);
+	if(vert_streets[y] == 'v')
+		search(x+1,y);
+
+}
+
+bool check_if_visited_all()
+{
+	bool visited_all = true;
+
+	for(int i=0;i<horizontal;i++)
 	{
-		dfs(a,b+1);
-		
-		if(street_UD[a] == '^')
-			dfs(a-1,b);
-		else
-			dfs(a+1,b);
+		for(int j=0;j<vertical;j++)
+		{
+			if(!visited[i][j])
+			{
+				visited_all = false;
+				break;
+			}
+		}
+		if(!visited_all)
+			break;
 	}
-	else if(street_LR[b] == '<')
+
+	return visited_all;
+}
+
+void clear_array()
+{
+	for(int i=0;i<horizontal;i++)
 	{
-		dfs(a,b-1);
-		if(street_UD[a] == '^')
-			dfs(a-1,b);
-		else
-			dfs(a+1,b);
+		for(int j=0;j<vertical;j++)
+			visited[i][j] = false;
 	}
-	
+}
+
+void print_array()
+{
+	for(int i=0;i<horizontal;i++)
+	{
+		for(int j=0;j<vertical;j++)
+			cout << visited[i][j] << " ";
+		cout << endl;
+	}
 }
 int main()
 {
-	int a,b;
-	memset(visited,0,sizeof(visited));
+	cin >> horizontal >> vertical;
 
-	cin >> a >> b;
+	for(int i=0;i<horizontal;i++)
+		cin >> hor_streets[i];
 
-	x = b;
-	y = a;
+	for(int i=0;i<vertical;i++)
+		cin >> vert_streets[i];
 
-	for(int i=0;i<x;i++)
-		cin >> street_LR[i];
-	
-	for(int i=0;i<y;i++)
-		cin >> street_UD[i];
-
-	if
-	( 
-		(street_UD[0] == '^' && street_LR[0] == '<') 	 ||
-		(street_UD[x-1] == 'v' && street_LR[y-1] == '>') ||
-		(street_UD[0] == 'v' && street_LR[y-1] == '<')   ||
-		(street_UD[x-1] == '^' && street_LR[0] == '>')
-	)
+	bool sol = true;
+	for(int i=0;i<horizontal;i++)
 	{
-		cout << "NO" << endl;
-		return 0;
-	}
-	else
-	{
-		for(int i=0;i<x;i++)
+		for(int j=0;j<vertical;j++)
 		{
-			for(int j=0;j<y;j++)
-			{
-				dfs(i,j);
-				for(int k=0;k<y;k++)
-				{
-					for(int l=0;l<x;l++)
-					{
-						cout << visited[k][l] << " ";
-					}
-					cout << endl;
-				}
-
-				memset(visited,0,sizeof(visited));
-				if(vcount != x*y)
-				{
-					cout << "NO" << endl;
-				}
-
-			}
+			clear_array();
+			search(i,j);
+			sol &= check_if_visited_all();
 		}
 	}
 
-	cout << "YES" << endl;
+	if(sol)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
 	return 0;
 }
