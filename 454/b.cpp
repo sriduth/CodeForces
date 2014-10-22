bedd1d0ba9a4ddbd9ddc6c15977ad7634b4dba92
Sriduth Jayhari <sriduth.jayhari@gmail.com>
2014-10-22 19:54:24 +0530

    Added new

--- 454/b.cpp
+++ 454/b.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
-#include <list>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int main()
 {
-	cin.sync_with_stdio(false);
-	list<int> data;
-	int T,temp;
+	int n;
 
-	cin >> T;
+	cin >> n;
 
-	for(int i=0;i<T;i++){
-		cin >> temp;
-		data.push_back(temp);
-	}
+	int seq[n];
 
-	
-	for(int i=0;i<T;i++)
+	for(int i=0;i<n;i++)
+		cin >> seq[i];
+
+	int sort_break = 0;
+	for(int i=0;i<n-1;i++)
 	{
-		if(is_sorted(data.begin(),data.end()))
+		if(seq[i] > seq[i+1])
 		{
-			cout << i << endl;
-			return 0;
-		}
-
-		else{
-			data.push_front(data.back());
-			data.pop_back();
+			sort_break = i+1;
+			break;
 		}
 	}
 
-	cout << "-1" << endl;
-	return 0;
+	vector<int> new_seq;
+	for(int i=sort_break;i<n+sort_break;i++)
+		new_seq.push_back(seq[i%n]);
+	
+	if(is_sorted(new_seq.begin(),new_seq.end()))
+	{
+		cerr << "Sorted" << endl;
+		if(sort_break == 0)
+			cout << '0' << endl;
+		else
+			cout << n - sort_break << endl;
+
+	}
+	else
+	{
+		cerr << "Not Sorted" << endl;
+		cout << "-1" << endl;
+	}
 }
